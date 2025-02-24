f5caf45f6b881c691786741a6a88b1d30973a3fa
EC2 Default User <ec2-user@ip-172-31-28-246.ec2.internal>
2025-02-24 05:54:10 +0000

    4 added & 1 removed

--- sbi.py
+++ sbi.py
@@ -1,2 +1,5 @@
-git add – adding a required file (single)
 git add 1
+git status – to check the file status
+git commit -m “commit message” (files available in local repository)
+git log – it shows commit history
+git log --oneline – shows commit history in single line.
