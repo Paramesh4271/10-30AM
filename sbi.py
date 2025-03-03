22572825b119cb8951ed955365817cb7090cc816
Paramesh4271 <127423148+Paramesh4271@users.noreply.github.com>
2025-03-03 10:43:00 +0530

    adding docker network

--- sbi.py
+++ sbi.py
@@ -19,3 +19,21 @@ MAINTAINER Name <email@example.com>
 MAINTAINER John Doe <john@example.com>
 Instead, use LABEL:
 LABEL maintainer="John Doe <john@example.com>"
+Docker Network
+===============
+Bridge
+The bridge driver creates an internal network within a single Docker host containers placed within this network can communicate with each other but are isolated from containers, not on the internal network, bridge is the default driver when running single containers or when using docker-compose.
+
+Host
+When using the host driver, the container shares the network stack of the Docker host appearing as if the container is the host itself, from a networking perspective.
+
+None
+The none driver simply disables networking for a container, making it isolated from other containers.
+
+sudo hostnamectl set-hostname <required name>
+sudo apt install net-tools -y
+ifconfig
+sudo docker network ls
+sudo docker run -itd --name <container name> -p <Container port:host port> <image name>
+sudo docker run -itd --name <container name> --net host <image name>
+sudo docker run -itd --name <container name> --net none <image name>
